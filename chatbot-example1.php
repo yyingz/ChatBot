f6f81a5df3e3242dbc5f07aa200196dc15330a0d
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-19 17:01:27 +0700

    Edit
    Code Reply Code

--- chatbot-example1.php
+++ chatbot-example1.php
@@ -9,7 +9,7 @@ $bot = new BOT_API($channelSecret, $access_token);
 	
 if (!empty($bot->isEvents)) {
 		
-    $bot->replyMessageNew($bot->replyToken, json_encode($bot->message));
+    $bot->replyMessageNew('Hello World !!');
 
     if ($bot->isSuccess()) {
         echo 'Succeeded!';
