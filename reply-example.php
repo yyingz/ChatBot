465b09d6a05d293fc1b667d7d812dec9f9dd4e4a
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-19 17:25:57 +0700

    Add line 60

--- reply-example.php
+++ reply-example.php
@@ -54,6 +54,12 @@
         $arrayPostData['messages'][1]['stickerId'] = "131";
         replyMsg($arrayHeader,$arrayPostData);
     }
+    else {
+        $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+        $arrayPostData['messages'][0]['type'] = "text";
+        $arrayPostData['messages'][0]['text'] = "พิมพ์ให้มันถูกซิ!!";
+        replyMsg($arrayHeader,$arrayPostData);
+    }
 function replyMsg($arrayHeader,$arrayPostData){
         $strUrl = "https://api.line.me/v2/bot/message/reply";
         $ch = curl_init();
