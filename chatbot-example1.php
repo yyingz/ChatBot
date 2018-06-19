8c1f3a486f49c40ecb4c3b2f54c1248b73ae5e31
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-19 17:10:32 +0700

    Edit Test send message

--- chatbot-example1.php
+++ chatbot-example1.php
@@ -6,7 +6,10 @@ $channelSecret = '4d2466fe67c5096d309d0ac512c245e7';
 $access_token  = 'w+lIPN3ILj/c/+qkzROqMAHg53BSJJTTz2tLw2m0IkIz4Qfi+sdFp3TDb0XlRdV/KivRIKFmtS43Xfw8uZfsNN/6kg/qiCZt2yF7Ve0+3YfIBX8PyNXKZ9z0571uVuapokc/bLdx4XtcbmP8fcSnrwdB04t89/1O/w1cDnyilFU=';
 
 $bot = new BOT_API($channelSecret, $access_token);
-	
+
+if (!empty($bot->isEvents)) {
+	$bot->sendMessageNew('U3a8884da940f2cb1dbbf2c30a69bd4f3', 'Not Emtry!!');
+}
 $bot->sendMessageNew('U3a8884da940f2cb1dbbf2c30a69bd4f3', 'Hello World !!');
 
 if ($bot->isSuccess()) {
