570a9a78e32c15e0c5ba791bf34625bf7a61b80f
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-20 16:13:25 +0700

    Update reply-example.php

--- reply-example.php
+++ reply-example.php
@@ -23,7 +23,7 @@
     if($message == "หิว" || "แนะนำร้านอาหาร" || "ร้านอาหาร" || "กินไรดี" || "หิวข้าว"){
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = "https://map.nostramap.com/NostraMap/?layer/sea2018,feed/th";
+        $arrayPostData['messages'][0]['text'] = "https://map.nostramap.com/NostraMap/?layer/michelin,feed/th";
         replyMsg($arrayHeader,$arrayPostData);
     }
     #ที่เที่ยว
@@ -31,7 +31,6 @@
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
         $arrayPostData['messages'][0]['text'] = "https://map.nostramap.com/NostraMap/?layer/sea2018,feed/th";
-        $arrayPostData['messages'][1]['type'] = "text";
         $arrayPostData['messages'][1]['text'] = "https://map.nostramap.com/NostraMap/?layer/fruit2018,feed/th";
         replyMsg($arrayHeader,$arrayPostData);
     }
