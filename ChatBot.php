313325b273f37af7bfc849099f0d2aaf3074f5e5
yyingz <40383818+yyingz@users.noreply.github.com>
2018-06-21 13:46:25 +0700

    Add ChatBot.php

---
+++ ChatBot.php
@@ -0,0 +1,52 @@
+<?php
+    $accessToken = "Lq8/yqdrOY4yfgWeUAP2rFojWCVox5J2FwhyxRYbQ6quFzDcc8chiA/hdHferUNPKivRIKFmtS43Xfw8uZfsNN/6kg/qiCZt2yF7Ve0+3Yeo+99ke6oEd1Cb5/GcwNDDnfKTIptA1wGdBU+8bmZIPwdB04t89/1O/w1cDnyilFU=";//copy Channel access token ตอนที่ตั้งค่ามาใส่
+    
+    $content = file_get_contents('php://input');
+    $arrayJson = json_decode($content, true);
+    
+    $arrayHeader = array();
+    $arrayHeader[] = "Content-Type: application/json";
+    $arrayHeader[] = "Authorization: Bearer {$accessToken}";
+    
+    //รับข้อความจากผู้ใช้
+    
+    $message = $arrayJson['events'][0]['message']['text'];
+
+    switch($message) {
+        case "สวัสดี":
+            $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+            $arrayPostData['messages'][0]['type'] = "text";
+            $arrayPostData['messages'][0]['text'] = "สวัสดีจ้าาา";
+            replyMsg($arrayHeader,$arrayPostData);
+            break;
+        case "Travel":
+            $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+            $arrayPostData['messages'][0]['type'] = "text";
+            $arrayPostData['messages'][0]['text'] = "https://map.nostramap.com/NostraMap/?layer/midyear2018,feed/th";
+            $arrayPostData['messages'][1]['type'] = "text";
+            $arrayPostData['messages'][1]['text'] = "https://map.nostramap.com/NostraMap/?layer/sea2018,feed/th";
+            replyMsg($arrayHeader,$arrayPostData);
+            break;
+        default:
+            $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
+            $arrayPostData['messages'][0]['type'] = "text";
+            $arrayPostData['messages'][0]['text'] = "พิมพ์ให้ถูกซิ!!!";
+            replyMsg($arrayHeader,$arrayPostData);
+            break;
+    }
+
+function replyMsg($arrayHeader,$arrayPostData){
+        $strUrl = "https://api.line.me/v2/bot/message/reply";
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,$strUrl);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $arrayHeader);    
+        curl_setopt($ch, CURLOPT_POSTFIELDS,json_encode($arrayPostData));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $result = curl_exec($ch);
+        curl_close ($ch);
+    }
+   exit;
+?>
